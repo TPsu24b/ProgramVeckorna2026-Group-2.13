e72c581c4a584d423b631919ded7c3c122b8b2a1
TPsu24b <theo.palm@elev.ga.lbs.se>
2026-01-13 13:58:21 +0100

    Merge

--- ProgVeck2026-Group-2.13/Assets/Scripts/Triggers/SceneTrigger.cs
+++ ProgVeck2026-Group-2.13/Assets/Scripts/Triggers/SceneTrigger.cs
@@ -4,11 +4,14 @@ using UnityEngine.InputSystem;
 public class SceneTrigger : MonoBehaviour
 {
     [SerializeField] private ASyncLoader sceneLoader;
+    [SerializeField] SaveManager saveManager;
     public string sceneToLoad;
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
+            if(saveManager != null)
+                saveManager.SaveData(sceneToLoad);
             sceneLoader.LoadLevelBtn(sceneToLoad);
         }
     }

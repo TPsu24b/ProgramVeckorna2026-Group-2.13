0571748cb52715c1429aec8ff8ab0b0ed7f92844
AlexH-LBS <alexander.holmstedt@elev.ga.lbs.se>
2026-01-15 21:54:28 +0100

    github be struggling so this pushed later

---
+++ ProgVeck2026-Group-2.13/Assets/Scripts/Bluetooth Devices/laptop.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class laptop : MonoBehaviour
+{
+    [SerializeField] GameObject interact, text;
+    bool inRange = false, isOpen;
+    [SerializeField] InputActionReference interaction;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inRange == true)
+        {
+            if (interaction.action.WasPerformedThisFrame())
+            {
+                
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        interact.SetActive(true);
+        inRange = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        interact.SetActive(false);
+        inRange = false;
+    }
+}
